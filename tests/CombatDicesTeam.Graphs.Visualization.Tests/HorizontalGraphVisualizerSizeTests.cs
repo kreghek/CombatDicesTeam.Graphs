d10c42a348e83140261752ac2f7eb89dd38d2e61
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-04-26 00:30:48 +0700

    Add test to check vertical layouts

--- tests/CombatDicesTeam.Graphs.Visualization.Tests/HorizontalGraphVisualizerSizeTests.cs
+++ tests/CombatDicesTeam.Graphs.Visualization.Tests/HorizontalGraphVisualizerSizeTests.cs
@@ -122,4 +122,41 @@ public sealed class HorizontalGraphVisualizerSizeTests
         var xOrderedLayouts = layouts.OrderBy(x => x.Position.X).Select(x=>x.Position).ToArray();
         xOrderedLayouts.Should().BeEquivalentTo(new[] { new Position(0, 0), new Position(NODE_SIZE, 0) });
     }
+
+    /// <summary>
+    /// Test checks the visualizer layouts node horizontally using node size from config.
+    /// </summary>
+    [Test]
+    public void Create_GraphWithTwoRoots_ReturnsLayoutsPlacedVerticallyUsingSize()
+    {
+        // ARRANGE
+
+        const int NODE_SIZE = 3;
+
+        var visualizer = new HorizontalGraphVisualizer<int>();
+
+        var graphMock = new Mock<IGraph<int>>();
+
+        var root1 = Mock.Of<IGraphNode<int>>(n => n.Payload == 0);
+        var root2 = Mock.Of<IGraphNode<int>>(n => n.Payload == 1);
+
+        graphMock.Setup(x => x.GetAllNodes()).Returns(new[] { root1, root2 });
+        graphMock.Setup(x => x.GetNext(It.Is<IGraphNode<int>>(n => n == root1)))
+            .Returns(ArraySegment<IGraphNode<int>>.Empty);
+        graphMock.Setup(x => x.GetNext(It.Is<IGraphNode<int>>(n => n == root2)))
+            .Returns(ArraySegment<IGraphNode<int>>.Empty);
+
+        var graph = graphMock.Object;
+
+        var layoutConfig = Mock.Of<ILayoutConfig>(x => x.NodeSize == NODE_SIZE);
+
+        // ACT
+
+        var layouts = visualizer.Create(graph, layoutConfig);
+
+        // ASSERT
+
+        var yOrderedLayouts = layouts.OrderBy(x => x.Position.Y).Select(x => x.Position).ToArray();
+        yOrderedLayouts.Should().BeEquivalentTo(new[] { new Position(0, 0), new Position(0, NODE_SIZE) });
+    }
 }
