5237b6dca42bcd39f4046f716f7dc870b4eaa583
Pavel Kurkutov <pkurkutov@ptsecurity.com>
2023-04-27 16:20:42 +0700

    Simplify algorithm

--- src/CombatDicesTeam.Graphs.Visualization/HorizontalGraphVisualizer.cs
+++ src/CombatDicesTeam.Graphs.Visualization/HorizontalGraphVisualizer.cs
@@ -35,25 +35,7 @@ public sealed class HorizontalGraphVisualizer<TValueData> : IGraphNodeVisualizer
         return nodesOpenList;
     }
 
-    private static IEnumerable<IReadOnlyList<T>> GetPermutations<T>(IReadOnlyList<T> list, int length)
-    {
-        if (length == 1)
-        {
-            return list.Select(t => new[] { t });
-        }
-
-        var permutationsMinus1 = GetPermutations(list, length - 1);
-        return permutationsMinus1
-            .SelectMany(t => list.Where(e => !t.Contains(e)),
-                (t1, t2) => t1.Concat(new[] { t2 }).ToArray());
-    }
-
-    private static IEnumerable<IReadOnlyList<T>> GetPermutations<T>(IReadOnlyList<T> list)
-    {
-        return GetPermutations(list, list.Count);
-    }
-
-    private static (IReadOnlyList<IReadOnlyList<IGraphNode<TValueData>>> TotalLevels, bool Success) CollectLevelInner(
+    private static IReadOnlyList<IReadOnlyList<IGraphNode<TValueData>>> CollectLevelInner(
         IGraph<TValueData> graph,
         IReadOnlyCollection<IGraphNode<TValueData>> stableCurrentLevelNodes,
         IReadOnlyList<IReadOnlyList<IGraphNode<TValueData>>> currentTotalLevels)
@@ -62,83 +44,39 @@ public sealed class HorizontalGraphVisualizer<TValueData> : IGraphNodeVisualizer
 
         if (!nextLevelNodes.Any())
         {
-            return (currentTotalLevels, true);
+            return currentTotalLevels;
         }
-
-        var nextLevelNodesPerms = GetPermutations(nextLevelNodes);
-
-        foreach (var nextLevelNodesPerm in nextLevelNodesPerms)
-        {
-            var nextHaveSameOrder = CheckNextLevelHaveSameOrder(
-                graph,
-                stableCurrentLevelNodes,
-                nextLevelNodesPerm);
-
-            if (!nextHaveSameOrder)
-            {
-                // Next level nodes is in wrong order.
-                // Try next permutation.
-                continue;
-            }
-
-            var totalList = new List<IReadOnlyList<IGraphNode<TValueData>>>(currentTotalLevels)
-            {
-                nextLevelNodesPerm
-            };
-            var (totalLevels, success) = CollectLevelInner(graph, nextLevelNodesPerm, totalList);
-
-            if (success)
-            {
-                return (totalLevels, true);
-            }
-        }
-
-        return (currentTotalLevels, false);
-    }
-
-    private static IReadOnlyList<IReadOnlyList<IGraphNode<TValueData>>> CollectLevels(IGraph<TValueData> graph,
-        IReadOnlyList<IGraphNode<TValueData>> rootNodes)
-    {
-        var rootNodesPerms = GetPermutations(rootNodes, rootNodes.Count);
-
-        foreach (var rootNodesPerm in rootNodesPerms)
-        {
-            var totalList = new List<IReadOnlyList<IGraphNode<TValueData>>>
-            {
-                rootNodesPerm
-            };
-
-            var (totalLevels, success) = CollectLevelInner(graph, rootNodesPerm, totalList);
-
-            if (success)
-            {
-                return totalLevels;
-            }
-        }
-
-        throw new InvalidOperationException();
-    }
-
-    private static bool CheckNextLevelHaveSameOrder(
-        IGraph<TValueData> graph,
-        IReadOnlyCollection<IGraphNode<TValueData>> currentLevelNodes,
-        IReadOnlyCollection<IGraphNode<TValueData>> nextLevelNodes)
-    {
-        var weightedCurrentLevelNodes = currentLevelNodes.Select((x, i) => (x, i)).ToArray();
+        
+        var weightedCurrentLevelNodes = stableCurrentLevelNodes.Select((x, i) => (x, i)).ToArray();
         var weightedNextLevelNodes = nextLevelNodes.Select(x => new
         {
             Node = x,
             Weight = CalculateChildWeight(graph, x, weightedCurrentLevelNodes)
         }).ToArray();
 
-        var nextLevelNodeWeights = weightedNextLevelNodes.Select(x => x.Weight).ToArray();
-
-        return CheckListIsOrdered(nextLevelNodeWeights);
+        var orderedWeightedNextLevelNodes = weightedNextLevelNodes.OrderBy(x => x.Weight).Select(x => x.Node).ToArray();
+        
+        var totalList = new List<IReadOnlyList<IGraphNode<TValueData>>>(currentTotalLevels)
+        {
+            orderedWeightedNextLevelNodes.ToArray()
+        };
+        
+        var totalLevels = CollectLevelInner(graph, orderedWeightedNextLevelNodes, totalList);
+        
+        return totalLevels;
     }
 
-    private static bool CheckListIsOrdered(IReadOnlyList<double> list)
+    private static IReadOnlyList<IReadOnlyList<IGraphNode<TValueData>>> CollectLevels(IGraph<TValueData> graph,
+        IReadOnlyList<IGraphNode<TValueData>> rootNodes)
     {
-        return !list.Where((t, i) => i > 0 && t < list[i - 1]).Any();
+        var totalList = new List<IReadOnlyList<IGraphNode<TValueData>>>
+        {
+            rootNodes
+        };
+        
+        var totalLevels = CollectLevelInner(graph, rootNodes, totalList);
+
+        return totalLevels;
     }
 
     /// <summary>
