dbe5d1cfec74d08b851abe86c9b61cf7c26780f5
Pavel Kurkutov <pkurkutov@ptsecurity.com>
2023-04-27 10:47:01 +0700

    Expand alignment tests

--- tests/CombatDicesTeam.Graphs.Visualization.Tests/HorizontalGraphVisualizerAlignmentTests.cs
+++ tests/CombatDicesTeam.Graphs.Visualization.Tests/HorizontalGraphVisualizerAlignmentTests.cs
@@ -3,10 +3,13 @@
 public sealed class HorizontalGraphVisualizerAlignmentTests
 {
     /// <summary>
-    /// Test checks the visualizer layouts node between two related.
+    /// Test checks the visualizer layouts child node opposite related parents.
     /// </summary>
     [Test]
-    public void Create_TowWays_ReturnsChildrenLayoutsOppositeParents()
+    [TestCaseSource(
+        typeof(HorizontalGraphVisualizerTestCases.HorizontalGraphVisualizerTestCases),
+        nameof(HorizontalGraphVisualizerTestCases.HorizontalGraphVisualizerTestCases.NodePerms2))]
+    public void Create_TowWays_ReturnsChildrenLayoutsOppositeParents(int[] indexes)
     {
         // ARRANGE
 
@@ -22,13 +25,85 @@ public sealed class HorizontalGraphVisualizerAlignmentTests
         var child1 = Mock.Of<IGraphNode<int>>(n => n.Payload == CHILD1_PAYLOAD);
         var child2 = Mock.Of<IGraphNode<int>>(n => n.Payload == CHILD2_PAYLOAD);
 
-        graphMock.Setup(x => x.GetAllNodes()).Returns(new[] { root1, root2, child1, child2 });
+        var nodeList = GetNodeListByIndexes(new[] { root1, root2, child1, child2 }, indexes);
+
+        graphMock.Setup(x => x.GetAllNodes()).Returns(nodeList);
 
         graphMock.Setup(x => x.GetNext(It.Is<IGraphNode<int>>(n => n == root1)))
             .Returns(new[] { child1 });
 
         graphMock.Setup(x => x.GetNext(It.Is<IGraphNode<int>>(n => n == root2)))
             .Returns(new[] { child2 });
+
+        graphMock.Setup(x => x.GetNext(It.Is<IGraphNode<int>>(n => n == child1 || n == child2)))
+            .Returns(ArraySegment<IGraphNode<int>>.Empty);
+
+        var graph = graphMock.Object;
+
+        var visualizer = new HorizontalGraphVisualizer<int>();
+
+        const int NODE_SIZE = 1;
+        var layoutConfig = Mock.Of<ILayoutConfig>(x => x.NodeSize == NODE_SIZE);
+
+        // ACT
+
+        var layouts = visualizer.Create(graph, layoutConfig);
+
+        // ASSERT
+
+        var root1Layout = layouts.Single(x => x.Node.Payload == ROOT1_PAYLOAD);
+        var child1Layout = layouts.Single(x => x.Node.Payload == CHILD1_PAYLOAD);
+
+        child1Layout.Position.Y.Should().Be(root1Layout.Position.Y);
+        
+        var root2Layout = layouts.Single(x => x.Node.Payload == ROOT2_PAYLOAD);
+        var child2Layout = layouts.Single(x => x.Node.Payload == CHILD2_PAYLOAD);
+
+        child2Layout.Position.Y.Should().Be(root2Layout.Position.Y);
+    }
+
+    private static IReadOnlyCollection<IGraphNode<T>> GetNodeListByIndexes<T>(IReadOnlyList<IGraphNode<T>> graphNodes, IEnumerable<int> indexes)
+    {
+        return indexes.Select(index => graphNodes[index]).ToList();
+    }
+
+    /// <summary>
+    /// Test checks the visualizer layouts child node opposite related parents.
+    /// </summary>
+    [Test]
+    [TestCaseSource(
+        typeof(HorizontalGraphVisualizerTestCases.HorizontalGraphVisualizerTestCases),
+        nameof(HorizontalGraphVisualizerTestCases.HorizontalGraphVisualizerTestCases.NodePermsIsolated))]
+    public void Create_TowWaysAndIsolatedRoot_ReturnsChildrenLayoutsOppositeParents(int[] indexes)
+    {
+        // ARRANGE
+
+        var graphMock = new Mock<IGraph<int>>();
+
+        const int ROOT1_PAYLOAD = 0;
+        const int ROOT2_PAYLOAD = 1;
+        const int ROOT_ISOLATED_PAYLOAD = 10;
+        const int CHILD1_PAYLOAD = 2;
+        const int CHILD2_PAYLOAD = 3;
+
+        var root1 = Mock.Of<IGraphNode<int>>(n => n.Payload == ROOT1_PAYLOAD);
+        var root2 = Mock.Of<IGraphNode<int>>(n => n.Payload == ROOT2_PAYLOAD);
+        var rootIsolated = Mock.Of<IGraphNode<int>>(n => n.Payload == ROOT_ISOLATED_PAYLOAD);
+        var child1 = Mock.Of<IGraphNode<int>>(n => n.Payload == CHILD1_PAYLOAD);
+        var child2 = Mock.Of<IGraphNode<int>>(n => n.Payload == CHILD2_PAYLOAD);
+        
+        var nodeList = GetNodeListByIndexes(new[] { root1, root2, child1, child2, rootIsolated }, indexes);
+
+        graphMock.Setup(x => x.GetAllNodes()).Returns(nodeList);
+
+        graphMock.Setup(x => x.GetNext(It.Is<IGraphNode<int>>(n => n == root1)))
+            .Returns(new[] { child1 });
+
+        graphMock.Setup(x => x.GetNext(It.Is<IGraphNode<int>>(n => n == root2)))
+            .Returns(new[] { child2 });
+        
+        graphMock.Setup(x => x.GetNext(It.Is<IGraphNode<int>>(n => n == rootIsolated)))
+            .Returns(ArraySegment<IGraphNode<int>>.Empty);
 
         graphMock.Setup(x => x.GetNext(It.Is<IGraphNode<int>>(n => n == child1 || n == child2)))
             .Returns(ArraySegment<IGraphNode<int>>.Empty);
---
+++ tests/CombatDicesTeam.Graphs.Visualization.Tests/HorizontalGraphVisualizerTestCases/HorizontalGraphVisualizerTestCases.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+
+namespace CombatDicesTeam.Graphs.Visualization.Tests.HorizontalGraphVisualizerTestCases;
+
+public static class HorizontalGraphVisualizerTestCases
+{
+    public static IEnumerable NodePerms2
+    {
+        get
+        {
+            var indexes = Enumerable.Range(0, 4).ToArray();
+
+            var perms = GetPermutations(indexes, indexes.Length);
+
+            foreach (var perm in perms)
+            {
+                yield return new TestCaseData(perm.ToArray());
+            }
+        }
+    }
+    
+    public static IEnumerable NodePermsIsolated
+    {
+        get
+        {
+            var indexes = Enumerable.Range(0, 5).ToArray();
+
+            var perms = GetPermutations(indexes, indexes.Length);
+
+            foreach (var perm in perms)
+            {
+                yield return new TestCaseData(perm.ToArray());
+            }
+        }
+    }
+    
+    private static IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length)
+    {
+        if (length == 1) return list.Select(t => new [] { t });
+
+        var materializedList = list as T[] ?? list.ToArray();
+        return GetPermutations(materializedList, length - 1)
+            .SelectMany(t => materializedList.Where(e => !t.Contains(e)),
+                (t1, t2) => t1.Concat(new [] { t2 }));
+    }
+}
