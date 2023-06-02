a7f9565e66eedd78daa718d057a97651c267ecf2
Pavel Kurkutov <kurkutovpa@gmail.com>
2023-06-02 14:33:36 +0700

    Remove visualization

--- src/CombatDicesTeam.Graphs.Visualization/GraphNodeControl.cs
+++
@@ -1,7 +0,0 @@
-namespace CombatDicesTeam.Graphs.Visualization;
-
-/// <summary>
-/// Base implementation of node layput.
-/// </summary>
-public sealed record GraphNodeLayout<TNodePayload>
-    (IGraphNode<TNodePayload> Node, Position Position, Size Size) : IGraphNodeLayout<TNodePayload>;
